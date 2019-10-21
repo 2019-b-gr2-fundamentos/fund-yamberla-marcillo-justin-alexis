08f7213db084b81f7893b3db724a6c6820a803c9
justinyamberla <55857299+justinyamberla@users.noreply.github.com>
2019-10-21 15:52:35 -0500

    Tipos de primitivas

---
+++ 02-variables-ts.ts
@@ -0,0 +1,17 @@
+//Variables primitivas
+const numero = 10
+const decimal = 12.10
+const respuestaTipoNumero = typeof numero;
+console.log(respuestaTipoNumero);
+console.log(typeof decimal);
+typeof numero
+const letra = 'a';
+const palabra = 'palabra'
+console.log(typeof letra);
+console.log(typeof palabra);
+const hijos = null
+console.log(typeof hijos);
+const casado = false;
+console.log(typeof casado);
+const fecha = new Date();
+console.log(typeof fecha);
