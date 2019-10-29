371cf66915b2f3ce81064c6d23a83e82f6868069
justinyamberla <55857299+justinyamberla@users.noreply.github.com>
2019-10-29 10:21:25 -0500

    Act.Deb2

--- Deber02/Variables.rb
+++ Deber02/Variables.rb
@@ -7,3 +7,15 @@
 #Las variables globales son las que se pueden acceder desde cualquier sitio y se escriben con el símbolo $ delante.
 
 #Las variables para instanciar un objeto se escriben con una arroba @ delante:
+
+numero = 7
+puts numero
+
+letra = 'J'
+puts letra
+
+boolean = true
+puts boolean
+
+frase = 'Hola Justin'
+puts frase
