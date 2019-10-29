164df26d7c7a8517984085bae851be41ac9b26bc
justinyamberla <55857299+justinyamberla@users.noreply.github.com>
2019-10-29 10:28:27 -0500

    JavaVariables

---
+++ JavaVariables/src/Variables.java
@@ -0,0 +1,26 @@
+//Variables Primitivas JAVA
+/*
+- byte: emplea un solo byte (8 bits) de almacenamiento; almacenar valores en el rango [-128, 127]
+- short: usa el doble de almacenamiento que el anterior; cualquier valor en el rango [-32.768, 32.767].
+- int: emplea 4 bytes de almacenamiento y es el tipo de dato entero más empleado; El rango de valores que puede representar va de -2^31 a (2^31)-1.
+- long: es el tipo entero de mayor tamaño, 8 bytes (64 bits); rango de valores desde -2^63 a (2^63)-1.
+- float: conocido como tipo de precisión simple, emplea un total de 32 bits; representa números en el rango de 1.4 x10^(-45) a 3.4028235 x10^38.
+- double: sigue un esquema de almacenamiento similar al anterior, pero usando 64 bits; Representa valores en el rango de 4.9 x10^(-324) a 1.7976931348623157 x10^308.
+- boolean: tiene la finalidad de facilitar el trabajo con valores "verdadero/falso". Los dos valores posibles de este tipo son true y false.
+- char: se utiliza para almacenar caracteres individuales (letras).
+*/
+
+
+public class Variables {
+    public static void main(String[] args) {
+
+        byte v1 =1;
+        short v2 = -30000;
+        int v3 = 52;
+        long v4 = 7894753;
+        double v6 = 1546.7896513265421;
+        boolean v7 = true;
+        char v8 = 'a';
+
+    }
+}
