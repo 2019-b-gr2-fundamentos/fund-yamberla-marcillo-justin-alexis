ae28c044ccbcd0ccbe88cb5b4ae4098e655c03fa
justinyamberla <55857299+justinyamberla@users.noreply.github.com>
2019-11-11 19:01:26 -0500

    Actualización

---
+++ 02-iterables/01-do_while.ts
@@ -0,0 +1,42 @@
+//do while
+/*let vecesQueHeComido = 0;
+do{
+    console.log("vamos a comer");
+    switch(vecesQueHeComido){
+        case 1:
+            console.log("Desayuno");
+            break;
+        case 2:
+            console.log("Almuerzo");
+            break;
+        case 3:
+            console.log("Merienda");
+            break;
+        default:
+            console.log("Gordito"); 
+            break;
+    }
+    vecesQueHeComido ++;
+}while(vecesQueHeComido < 3) */
+
+//while
+
+let vecesQueHeComido = 0;
+while(vecesQueHeComido < 3){
+    console.log("vamos a comer");
+    switch(vecesQueHeComido){
+        case 1:
+            console.log("Desayuno");
+            break;
+        case 2:
+            console.log("Almuerzo");
+            break;
+        case 3:
+            console.log("Merienda");
+            break;
+        default:
+            console.log("Gordito"); 
+            break;
+    }
+    vecesQueHeComido ++;
+}
